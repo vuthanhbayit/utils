f1f8fdd16815b9e398b2a576f6df7667dbe9f0d3
Vu Thanh Bay <vuthanhbayit@gmail.com>
2021-10-07 12:39:52 +0700

    feat(url): add is-url fn

--- src/style.ts
+++ src/style.ts
@@ -3,12 +3,12 @@ import { isDefined, isNumeric } from './is'
 /**
  * @param value
  * @return {string}
- * @example formatCssUnit(10) => '10px'
- * @example formatCssUnit('20.2') => '20.2px'
- * @example formatCssUnit('5rem') => '5rem'
+ * @example toCssUnit(10) => '10px'
+ * @example toCssUnit('20.2') => '20.2px'
+ * @example toCssUnit('5rem') => '5rem'
  */
 
-export const formatCssUnit = (value?: string | number) => {
+export const toCssUnit = (value?: string | number) => {
   if (!isDefined(value)) return undefined
 
   return isNumeric(value) ? `${value}px` : String(value)
