55456af72133939126be4222caa576726e523f62
Vu Thanh Bay <vuthanhbayit@gmail.com>
2021-10-14 10:52:21 +0700

    feat(style): add rgbToHex

--- src/style.ts
+++ src/style.ts
@@ -7,9 +7,18 @@ import { isDefined, isNumeric } from './is'
  * @example toCssUnit('20.2') => '20.2px'
  * @example toCssUnit('5rem') => '5rem'
  */
-
 export const toCssUnit = (value?: string | number) => {
   if (!isDefined(value)) return undefined
 
   return isNumeric(value) ? `${value}px` : String(value)
 }
+
+/**
+ * @param r: number
+ * @param g: number
+ * @param b: number
+ * @return {string}
+ * @example rgbToHex(0, 0, 0) => '#000000'
+ * @example rgbToHex(255, 255, 255) => '#ffffff'
+ */
+export const rgbToHex = (r: number, g: number, b: number) => `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`
