4951c142e09415e8ce22e26d44f05e5e1b89ed88
Vu Thanh Bay <vuthanhbayit@gmail.com>
2021-10-14 10:51:48 +0700

    fix(random): fix randomNumberInRange

--- src/random.ts
+++ src/random.ts
@@ -1,4 +1,4 @@
-export const getRandomInt = (min = 1, max = 10000) => {
+export const randomNumberInRange = (min = 1, max = 10000) => {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
