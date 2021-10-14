cdc29a2439e83d6ad888d85d8a5b95b55c8d1d45
Vu Thanh Bay <vuthanhbayit@gmail.com>
2021-10-14 11:00:55 +0700

    feat(math): add average

--- src/math.ts
+++ src/math.ts
@@ -1,7 +1,3 @@
-export function clamp(n: number, min: number, max: number) {
-  return Math.min(max, Math.max(min, n))
-}
-
 /**
  * @param array: T[]
  * @param transform: function
@@ -9,9 +5,13 @@ export function clamp(n: number, min: number, max: number) {
  * @example sum([1, 2, 3, 4, 5]) => 15
  * @example sum([{a: 1}, {a:2}, {a:3}], item => item.a) => 6
  */
-export function sum<T>(
-  array: T[],
-  transform = (props: T): number => props as unknown as number,
-) {
-  return array.reduce((accumulator, item) => accumulator + transform(item), 0)
-}
+export const sum = <T>(array: T[], transform = (props: T): number => props as unknown as number) => array.reduce((accumulator, item) => accumulator + transform(item), 0)
+
+/**
+ * @param array: T[]
+ * @param transform: function
+ * @return number
+ * @example average([1, 2, 3, 4, 5]) => 3
+ * @example average([{a: 1}, {a:2}, {a:3}], item => item.a) => 2
+ */
+export const average = <T>(array: T[], transform = (props: T): number => props as unknown as number) => sum(array, transform) / array.length
