a788f992a1cbee5a8de91281e006c72928c952ca
Vu Thanh Bay <vuthanhbayit@gmail.com>
2022-04-11 09:41:44 +0700

    fix(isEqual): side effect sort array value

--- src/is.ts
+++ src/is.ts
@@ -64,7 +64,7 @@ export const isEqual = <T>(value: T, other: T) => {
   if (typeof value !== typeof other) return false
 
   if (isArray(value) && isArray(other))
-    return value.sort().toString() === other.sort().toString()
+    return [...value].sort().toString() === [...other].sort().toString()
 
   if (isObject(value) && isObject(other)) {
     return (
